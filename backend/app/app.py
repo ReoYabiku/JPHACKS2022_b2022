48aadd6d27538ebb8654abd3ae1382a31c13424c
ReoYabiku <alcremikyu.z@gmail.com>
2022-10-13 19:59:33 +0900

    flaskAPIを外部からたたけるようにする

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
-import json
+from flask import *
+from flask_cors import CORS
+
+# import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def hello_world():
-    return json.dumps([{"message":"Hello, World!"}], indent=2)
+    # return json.dumps([{"message":"Hello, World!"}], indent=2)
+    return jsonify({"language": "python"})
