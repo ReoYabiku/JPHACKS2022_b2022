ba64726b4ed2529b42d6543e0267db3697d2e62c
ReoYabiku <alcremikyu.z@gmail.com>
2022-10-17 17:48:07 +0900

    クリップボードにコピーできる

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Modal.css";
+import { TiDeleteOutline } from "react-icons/ti";
 
 export default function Modal({showModal = true, setShowModal= f => f, codes = f => f}) {
   const removeModal = () => {
     setShowModal(false);
-  }
+  };
+  
+  const copyTextToClipboard = () => {
+    console.log(codes);
+    navigator.clipboard.writeText(codes.join("\n"));
+  };
+  
+  useEffect(() => {
+    var textarea = document.getElementById("textarea");
+    if (textarea != null) {
+      textarea.value = codes.join("\n");
+    }
+  });
 
   return (
     <>{
       showModal ? (
         <div className="overlay">
           <div className="modalContent">
-            <div className="removeWrapper">
-              <div className="removeBtn" onClick={removeModal}>X</div>
+            <div className="btnWrapper">
+              <div className="removeBtn" onClick={removeModal}>
+                <TiDeleteOutline size={"2rem"} />
+              </div>
             </div>
-            {codes.map((code, i) => <p key={i}>{code}</p>)}
+            <textarea id="textarea" cols={100} rows={20}>
+            </textarea>
+            <div className="btnWrapper">
+              <div className="copyBtn" onClick={copyTextToClipboard}>Copy</div>
+            </div>
           </div>
         </div>
       ) : (
-        <>
-          <div>none</div>
-        </>
+        <></>
       )
     }</>
   );
