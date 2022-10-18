da4191d9e34b1ca2b54b9af92e561f464f647359
ReoYabiku <alcremikyu.z@gmail.com>
2022-10-18 20:32:06 +0900

    トップページの見栄えを整える

--- frontend/src/components/molecules/Step.js
+++ frontend/src/components/molecules/Step.js
@@ -4,9 +4,11 @@ import "./Step.css";
 export default function Step({path="/about", imgPath = "logo192.png", num = 0, value = ""}) {
   return (
     <a href={path} className="step-container">
-      <img src={imgPath} alt={imgPath}></img>
-      <p className="step-number">{num}</p>
-      <p>{value}</p>
+      <img src={imgPath} alt={imgPath} width={"70px"} height={"70px"}></img>
+      <div className="step-title">
+        <p className="step-number">{num}</p>
+        <div className="step-sentense"><p>{value}</p></div>
+      </div>
     </a>
   );
 }
